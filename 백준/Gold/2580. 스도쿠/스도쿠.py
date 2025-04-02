1a9b02ec9c703dd73511f47300659f1b7d34d2cf
wntjsrl <seongi0407@gmail.com>
2025-04-02 22:15:08 +0900

    [Gold IV] Title: 스도쿠, Time: 2064 ms, Memory: 130992 KB -BaekjoonHub

---
+++ 백준/Gold/2580. 스도쿠/스도쿠.py
@@ -0,0 +1,32 @@
+def is_valid(x, y, num):
+    for i in range(9):
+        # 같은 열에 있거나, 같은 행에 있을 때
+        if arr[x][i] == num or arr[i][y] == num:
+            return False
+    # 3 x 3 안에 있을 때
+    nx, ny = 3 * (x // 3), 3 * (y // 3)
+    for i in range(3):
+        for j in range(3):
+            if arr[nx + i][ny + j] == num:
+                return False
+    return True
+
+def recur(cnt):
+    # 빈칸의 수만큼 재귀가 진행되면 종료
+    if cnt == len(blanks):
+        for i in range(9):
+            for j in range(9):
+                print(arr[i][j], end=" ")
+            print()
+        exit(0)
+    i, j = blanks[cnt]
+    for k in range(1, 10):
+        if is_valid(i, j, k):
+            arr[i][j] = k
+            recur(cnt + 1)
+            arr[i][j] = 0
+
+arr = [list(map(int, input().split())) for _ in range(9)]
+# 빈칸의 좌표가 들어있는 리스트
+blanks = [(i, j) for i in range(9) for j in range(9) if arr[i][j] == 0]
+recur(0)
