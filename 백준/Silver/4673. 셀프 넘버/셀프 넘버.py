b8525a8b3982b45a399a2247d92e18c684aed125
wntjsrl <seongi0407@gmail.com>
2025-04-12 16:41:50 +0900

    [Silver V] Title: 셀프 넘버, Time: 40 ms, Memory: 32488 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
@@ -1,15 +1,22 @@
-# 생성자를 통해 생성된 어떤 수를 반환하는 함수
-def calculate_sum(number):
-    return number + sum(map(int, str(number)))
+# self_number를 찾는 재귀함수
+def recur(number):
+    next_number = number + sum(map(int, str(number)))
+    # 종료 조건
+    if next_number > 10000:
+        return
+    # 이미 셀프 넘버가 아닐 때, 재귀함수 종료
+    if not self_number_list[next_number]:
+        return
+    self_number_list[next_number] = False
+    recur(next_number)
 
 # self_number_list를 수정할 함수
 def find_self_number():
     for i in range(1, 10001):
-        certain_number = calculate_sum(i)
-        if certain_number <= 10000:
-            self_number_list[certain_number] = False
+        if self_number_list[i]:
+            recur(i)
 
-# True면 셀프 넘버, False면 생성자 있음
+# self_number인지, 아닌지를 저장할 리스트
 self_number_list = [True] * 10001
 find_self_number()
 for i in range(1, 10001):
